82158a95b315da19cebb6197ea09934f4587290d
Hyn1010 <ngochuyen.nguyen0910@gmail.com>
2023-06-02 10:56:26 +0700

    New screens

--- Library_Management/Home.cs
+++ Library_Management/Home.cs
@@ -4,17 +4,328 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using FontAwesome.Sharp;
+using Library_Management;
+using Color = System.Drawing.Color;
 
 namespace Library_Management
 {
+
     public partial class Home : Form
     {
+
+        //Fields
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+        private Form currentChildForm;
+
+        //Constructor
         public Home()
         {
             InitializeComponent();
+            customizeDesing();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
+            //Form
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+        //Structs
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(24, 217, 170);
+        }
+
+        //Methods
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if (senderBtn != null)
+            {
+                DisableButton();
+                //Button
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+                //Left border button
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+                //Icon Current Child Form
+                iconCurrentChildForm.IconChar = currentBtn.IconChar;
+                iconCurrentChildForm.IconColor = color;
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(4, 4, 32);
+                currentBtn.ForeColor = Color.FromArgb(240, 236, 233);
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.FromArgb(240, 236, 233);
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentChildForm != null)
+            {
+                //open only form
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelDesktop.Controls.Add(childForm);
+            panelDesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitleChildForm.Text = childForm.Text;
+
+        }
+
+        private void customizeDesing()
+        {
+            panelKhoSachSubMenu.Visible = false;
+            panelDocGiaSubMenu.Visible = false;
+            panelDichVuSubMenu.Visible = false;
+            //...
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelKhoSachSubMenu.Visible == true)
+                panelKhoSachSubMenu.Visible = false;
+            if (panelDocGiaSubMenu.Visible == true)
+                panelDocGiaSubMenu.Visible = false;
+            if (panelDichVuSubMenu.Visible == true)
+                panelDichVuSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+
+
+        private void TrangChu_Click_1(object sender, EventArgs e)
+        {
+
+            //...
+            hideSubMenu();
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            Reset();
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
+            iconCurrentChildForm.IconChar = IconChar.House;
+            iconCurrentChildForm.IconColor = Color.FromArgb(24, 217, 170);
+            lblTitleChildForm.Text = "Trang chủ";
+        }
+        private void KhoSach_Click(object sender, EventArgs e)
+        {
+
+            showSubMenu(panelKhoSachSubMenu);
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormKhoSach());
+        }
+
+        #region PanelKhoSach
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+        #endregion
+
+        private void DocGia_Click(object sender, EventArgs e)
+        {
+
+            showSubMenu(panelDocGiaSubMenu);
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormDocGia());
+        }
+
+        #region PanelDocGia
+        private void button18_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        #endregion
+
+        private void DichVu_Click(object sender, EventArgs e)
+        {
+
+            showSubMenu(panelDichVuSubMenu);
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormDichVu());
+        }
+
+        #region PanelDichVu
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //...
+            hideSubMenu();
+        }
+
+        #endregion
+
+        private void ThongKe_Click(object sender, EventArgs e)
+        {
+
+            //...
+            hideSubMenu();
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormThongKe());
+
+        }
+
+        private void TuyChinh_Click(object sender, EventArgs e)
+        {
+
+            //...
+            hideSubMenu();
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormTuyChinh());
+        }
+
+        private void TroGiup_Click(object sender, EventArgs e)
+        {
+
+            //...
+            hideSubMenu();
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormHoTro());
+        }
+
+        private Form activeForm = null;
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Maximized;
+            else
+                WindowState = FormWindowState.Normal;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Remove transparent border in maximized state
+        private void FormMainMenu_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                FormBorderStyle = FormBorderStyle.None;
+            else
+                FormBorderStyle = FormBorderStyle.Sizable;
         }
     }
+
+
+
+    //...
+
 }
+
