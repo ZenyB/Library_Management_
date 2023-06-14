c2aec2c629fa1d4ebf7c8bce05c6244d89195c29
Faryn-03 <anhphuocdang@gmail.com>
2023-06-14 08:57:21 +0700

    Revert "Revert "XNPM""
    This reverts commit 0ddef91aa87bf008b8590c6599cabe886beac52d.

---
+++ Library_Management/Forms/XacNhanMS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management.Forms.Dich_Vu
+{
+    public partial class XacNhanMS : Form
+    {
+        public XacNhanMS()
+        {
+            InitializeComponent();
+        }
+    }
+}
